657bdd724870ace54b08d4924fc5fe1e2c1db000
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-09-14 15:41:19 -0700

    refactored the request so it performed a fetch on the entire site, rather than one book, also added the parse and setup my book model/pool utils

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -1,10 +1,11 @@
 const request = require('../lib/lib/request');
 
 describe('request function', () => {
-    it('makes a request to the Book of Basketball page and returns html', async() => {
+    it('makes a request to the book page and returns html', async() => {
         const document = await request();
         expect(
-          document.querySelector(".content p.price_color").textContent
-        ).toEqual('£44.84');
+          document.querySelector('.product_pod h3 a').textContent
+        ).toEqual('A Light in the ...');
     });
 });
+
