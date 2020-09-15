b366db70dc42ceabd421daaf8914bf2a0d62191f
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-09-15 14:35:16 -0700

    queue.js added

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -3,9 +3,8 @@ const request = require('../lib/request');
 describe('request function', () => {
   it('makes a request to the book page and returns html', async() => {
     const document = await request(1);
-    expect(
-      document.querySelector('.product_pod h3 a').textContent
-    ).toEqual('A Light in the ...');
+    expect(document.querySelector('.product_pod h3 a').textContent)
+      .toEqual('A Light in the ...');
   });
 });
 
